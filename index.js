61f73d3bd9dfaa51832b6286813ad0a83db8fa57
po12133 <vargascarlos12123@gmail.com>
2023-12-13 12:46:21 -0400

    se añadio el gitignore

--- index.js
+++
@@ -1,8 +0,0 @@
-const server = require('./src/app.js');
-const axios = require('axios');         
-const swaggerDocs = require("../API-clientes-Advantech/src/swagger.js")
-
-server.listen(3001, () => {
-  console.log('%s listening at 3001'); // eslint-disable-line no-console
-  swaggerDocs(server, 3001);
-});
